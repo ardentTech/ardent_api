afcb37c649c45528bd522e4fd6e1ffd897b970bf
Jonathan D. Baker <jonathan@ardent.tech>
2018-07-17 14:43:40 -0600

    add image preview to product

--- api/commerce/admin.py
+++ api/commerce/admin.py
@@ -1,10 +1,40 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.admin.widgets import AdminFileWidget
+from django.utils.safestring import mark_safe
 
 from .models import Product, ProductImage
 
 
+class AdminImageMixin(object):
+
+    def to_html(self, obj, height=100):
+        width = obj.width / (obj.height / height)
+        return mark_safe(
+            '<img src="{}" width="{}" height="{}">'.format(obj.url, width, height))
+
+
+class AdminImageWidget(AdminImageMixin, AdminFileWidget):
+
+    def render(self, name, value, attrs=None):
+        output = []
+        if value is not None:
+            output.append(self.to_html(value))
+        output.append(super().render(name, value, attrs))
+        return mark_safe("".join(output))
+
+
+class ProductImageForm(forms.ModelForm):
+
+    class Meta:
+        fields = "__all__"
+        model = ProductImage
+        widgets = {"image": AdminImageWidget}
+
+
 class ProductImageInline(admin.TabularInline):
 
+    form = ProductImageForm
     model = ProductImage
 
 
@@ -33,5 +63,32 @@ class ProductAdmin(admin.ModelAdmin):
     readonly_fields = ("created", "id", "updated",)
 
 
+class ProductImageAdmin(AdminImageMixin, admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            "fields": (
+                "image",
+                "product",
+            )
+        }),
+        ("Meta", {
+            "classes": ("grp-collapse",),
+            "fields": ("created", "id", "updated",)
+        })
+    )
+    list_display = (
+        "id",
+        "image_preview",
+        "product",
+        "created",
+        "updated",
+    )
+    list_filter = ()
+    readonly_fields = ("created", "id", "updated",)
+
+    def image_preview(self, obj):
+        return self.to_html(obj.image, height=75)
+
+
 admin.site.register(Product, ProductAdmin)
-admin.site.register(ProductImage)
+admin.site.register(ProductImage, ProductImageAdmin)
